daec10597f2f4d8fcf56ad3a4f6692163eeb2e39
Daniel Mora <105325261+DanM854@users.noreply.github.com>
2022-11-24 22:09:59 -0500

    Update Conexion.java

--- src/main/java/vista/Conexion.java
+++ src/main/java/vista/Conexion.java
@@ -2,18 +2,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package vista;
+package com.mycompany.basedatosfinal;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author julia
+ * @author echamorro
  */
-class Conexion {
-
-    Connection estableceConexion() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+public class Conexion {
+    Connection conectar= null;
+    String usuario="root";
+    String contrasena="123456";
+    String bd="Tablas";
+    String ip="Localhost";
+    String puerto="3306";
+    
+    String cadena="jdbc:mysql://"+ip+":"+puerto+"/"+bd;
+    
+    public Connection estableceConexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar=DriverManager.getConnection(cadena,usuario,contrasena);
+            JOptionPane.showMessageDialog(null,"Se conecto");
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"no se conecto"+e.toString());
+        }
+    return conectar;
     }
     
 }
