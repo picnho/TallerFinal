3cb1b5c28b0f98b2552b0253bfe4bcb861513c7a
Daniel Mora <105325261+DanM854@users.noreply.github.com>
2022-11-24 21:38:59 -0500

    conexion

--- src/main/java/com/mycompany/tallerfinal/TallerFinal.java
+++ src/main/java/com/mycompany/tallerfinal/TallerFinal.java
@@ -1,17 +1,36 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.mycompany.basedatosfinal;
 
-package com.mycompany.tallerfinal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author julia
+ * @author echamorro
  */
-public class TallerFinal {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Conexion {
+    Connection conectar= null;
+    String usuario="root";
+    String contrasena="123456";
+    String bd="Tablas";
+    String ip="Localhost";
+    String puerto="3306";
+    
+    String cadena="jdbc:mysql://"+ip+":"+puerto+"/"+bd;
+    
+    public Connection estableceConexion(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar=DriverManager.getConnection(cadena,usuario,contrasena);
+            JOptionPane.showMessageDialog(null,"Se conecto");
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"no se conecto"+e.toString());
+        }
+    return conectar;
     }
+    
 }
